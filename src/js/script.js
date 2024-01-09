4a4a10bafab7bba6fb79d91d8d913d1afdee157b
Sema Nur Selçuk <92156971+b2210356117@users.noreply.github.com>
2024-01-09 15:57:28 +0300

    9.01.24
    collision added
    controls fixed

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,10 @@
 import * as THREE from 'three';
 import * as DAT from 'dat.gui';
+import * as CANNON from 'cannon-es';
 import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader.js";
 import {Mesh, MeshBasicMaterial, TextureLoader} from "three";
 import {PointerLockControls} from "three/addons/controls/PointerLockControls.js";
+import Stats from "three/addons/libs/stats.module.js";
 //import {World} from 'cannon-es'
 
 // todo: add ui for speed and tuş atamaları
@@ -15,6 +17,11 @@ import {PointerLockControls} from "three/addons/controls/PointerLockControls.js"
 // stats.showPanel(0);
 // document.body.appendChild(stats.dom);
 
+const world = new CANNON.World({
+    //gravity: new CANNON.Vec3(-0.05, -0.05, 0)
+});
+const timeStep = 1/60;
+
 var urls = [
     'rsc/GLTF/Spaceship_FernandoTheFlamingo.gltf',
     'rsc/mikuGLTF/scene.gltf',
@@ -22,6 +29,7 @@ var urls = [
     'rsc/blueRock/scene.gltf',
     'rsc/maintRobot/scene.gltf',
 ]
+
 
 // import ship files
 const shipUrl = new URL(urls[2], import.meta.url);
@@ -45,111 +53,11 @@ var cameraControls = new PointerLockControls(camera, renderer.domElement);
 
 const assetLoader = new GLTFLoader();
 
-// adding spaceship
-var spaceship;
-var boundingBox;
-//const texture = new TextureLoader().load('rsc/triangleGLTF/textures');
-assetLoader.load(shipUrl.href, function (gltf) {
-    const model = gltf.scene;
-    model.castShadow = true;
-    model.receiveShadow = true;
-    // model.traverse((obj) => {
-    //     if(obj instanceof Mesh) {
-    //         obj.material = new MeshBasicMaterial({map: texture})
-    //         obj.receiveShadow = true;
-    //         obj.castShadow = true;
-    //     }
-    // })
-    scene.add(model);
-    model.position.set(0, 5, 0);
-    //model.scale.setScalar(2); // try 50 for miku
-    model.rotation.y = -1 * Math.PI;
-    spaceship = model;
-    //camera.lookAt(spaceship.position.x, spaceship.position.y, spaceship.position.z - 50);
 
-    // var bottomVec = new THREE.Vector3(spaceship.position.x -5, spaceship.position.y, -6);
-    // var topVec = new THREE.Vector3(spaceship.position.x +5, spaceship.position.y + 3, 6);
-    // boundingBox = new THREE.Box3(bottomVec, topVec);
-    // var size = new THREE.Vector3();
-    // var center = new THREE.Vector3();
-    // boundingBox.getSize(size);
-    // boundingBox.getCenter(center);
-    // console.log(size);
-    // console.log(center);
-    // console.log(spaceship.position.y);
-    //
-    // boundingBox.setFromObject(spaceship);
-    //
-    // var boundBoxHelper = new THREE.Box3Helper(boundingBox);
-    // scene.add(boundingBox);
-    // scene.add(boundBoxHelper);
-
-
-}, undefined, function (error) {
-    console.error(error);
-});
-
-// spy ship (taking inspiration from fromsoft >:))
-var spyShip;
-var spaceshipBox;
-assetLoader.load(shipUrl.href, function (gltf) {
-    spyShip = gltf.scene;
-    scene.add(spyShip);
-    spyShip.position.set(0, 5, 0);
-    spyShip.scale.setScalar(0.5); // try 50 for miku
-    spyShip.rotation.y = -1 * Math.PI;
-
-    var bottomVec = new THREE.Vector3(spaceship.position.x -5, spaceship.position.y, -6);
-    var topVec = new THREE.Vector3(spaceship.position.x +5, spaceship.position.y + 3, 6);
-    spaceshipBox = new THREE.Box3(bottomVec, topVec);
-    spaceshipBox.setFromObject(spyShip);
-
-    var boundBoxHelper = new THREE.Box3Helper(spaceshipBox);
-    scene.add(spaceshipBox);
-    //kascene.add(boundBoxHelper);
-
-});
-
-
-
-// adding first debris: a rock!
-var debris1;
-var debris1Box;
-assetLoader.load(debris1Url.href, function (gltf) {
-    debris1 = gltf.scene;
-    debris1.castShadow = true;
-    debris1.receiveShadow = true;
-    scene.add(debris1);
-    //debris1.scale.set(0.5);
-    debris1.position.set(0, 5, -500);
-
-    // bounding box for debris
-    debris1Box = new THREE.Box3().setFromObject(debris1);
-    var boundBoxHelper = new THREE.Box3Helper(debris1Box);
-    scene.add(debris1Box);
-    //scene.add(boundBoxHelper);
-})
-// var spyDebris;
-//
-// assetLoader.load(debris1Url.href, function (gltf) {
-//     spyDebris = gltf.scene;
-//     spyDebris.castShadow = true;
-//     spyDebris.receiveShadow = true;
-//     scene.add(spyDebris);
-//     //debris1.scale.set(0.5);
-//     spyDebris.position.set(0, 5, -500);
-//
-//     // bounding box for debris
-//     var bottomVec = new THREE.Vector3(spaceship.position.x -5, spaceship.position.y, -6);
-//     var topVec = new THREE.Vector3(spaceship.position.x +5, spaceship.position.y + 3, 6);
-//     debris1Box = new THREE.Box3(bottomVec, topVec);
-//     debris1Box.setFromObject(spyDebris);
-//
-//     var boundBoxHelper = new THREE.Box3Helper(debris1Box);
-//     scene.add(debris1Box);
-//     scene.add(boundBoxHelper);
-// })
-
+var spaceship, spaceshipBox;
+loadModel("spaceship", shipUrl.href, new THREE.Vector3(0, 5, 0), new THREE.Vector3(1, 1, 1), new THREE.Euler(0, -1 * Math.PI, 0), 5, true, true);
+var debris1, debris1Box;
+loadModel("debris1", debris1Url.href, new THREE.Vector3(0, 5, -500), new THREE.Vector3(1, 1, 1), new THREE.Euler(0, 0, 0), 20, true, true);
 
 // spaceship controls
 const controls = {
@@ -159,10 +67,14 @@ const controls = {
     right: false,
     up: false,
     down: false,
+    pause: false,
 
     speed: 0.02,
-    minSpeed: 0.02,
-    maxSpeed: 15,
+    minSpeed: -15,
+    maxSpeed: -300,
+    minRotation: -45 * Math.PI / 180,
+    maxRotation: 45 * Math.PI / 180,
+    totalRotation: { x:0, y:0, z:0 },
 
     boost: false,
     boostCoolDown: 20, // minimum seconds between two carried out boost commands
@@ -170,29 +82,22 @@ const controls = {
 };
 
 // add a grid to help us
-const gridHelper = new THREE.GridHelper(300000, 10000); // it's arguments are not necessary
+const gridHelper = new THREE.GridHelper(300000, 10000);
 scene.add(gridHelper);
 
 // let there be light
 const ambientLight = new THREE.AmbientLight(0xccffe6); // ambient light
-// try 0xccffe6 if this one doesn't work
 scene.add(ambientLight);
-
-// const directionalLight = new THREE.DirectionalLight();
-// scene.add(directionalLight);
-// directionalLight.castShadow = true;
-// const dLightHelper = new THREE.DirectionalLightHelper(directionalLight, 10);
-// scene.add(dLightHelper);
 
 const rightPLight = new THREE.PointLight(0xff3300, 10000*controls.speed);
 scene.add(rightPLight);
 const leftPLight = new THREE.PointLight(0xff3300, 10000*controls.speed);
 scene.add(leftPLight);
 
-const rPLightHelper = new THREE.PointLightHelper(rightPLight, 1);
-const lPLightHelper = new THREE.PointLightHelper(leftPLight, 1);
-scene.add(rPLightHelper);
-scene.add(lPLightHelper);
+// const rPLightHelper = new THREE.PointLightHelper(rightPLight, 1);
+// const lPLightHelper = new THREE.PointLightHelper(leftPLight, 1);
+// scene.add(rPLightHelper);
+// scene.add(lPLightHelper);
 
 rightPLight.position.set(-5,0,0);
 leftPLight.position.set(5,0,0);
@@ -206,123 +111,111 @@ gui.add(options, 'lightIntensity', 0.5, 2).onChange(function (e) {
 
 });
 
-// var rightplightCoord = {
-//     x: 0,
-//     y: 0,
-//     z: 0,
-// }
-// var leftplightCoord = {
-//     x: 0,
-//     y: 0,
-//     z: 0,
-// }
-// gui.add(rightplightCoord, 'x', -50, 50).onChange(function (e) {
-//     rightPLight.position.z = spaceship.position.z + e;
-// });
-
-//orbit.target = new THREE.Vector3(spaceship.position.x, spaceship.position.y, spaceship.position.z - 50);
 renderer.setClearColor(0x060d13); // dark blue for space
 
 function animate(time) {
-    if (spaceship) {
-        spaceshipMovement();
-        if (spyShip) {
-            spaceshipBox.setFromObject(spyShip);
-            //spaceshipBox.expandByScalar(5);
-        }
-        //console.log(spaceship.rotation.z);
-    }
+    if (!controls.pause) {
+        world.step(timeStep);
 
-    if(spyShip && debris1) {
-        if (spaceshipBox.intersectsBox(debris1Box)) {
-            console.log("INTERSECTION!!!!");
+
+        if (spaceship) {
+            spaceshipMovement();
+            spaceship.position.copy(spaceshipBox.position);
+            spaceship.quaternion.copy(spaceshipBox.quaternion);
         }
+        if (debris1) {
+            debris1.position.copy(debris1Box.position);
+            debris1.quaternion.copy(debris1Box.quaternion);
+        }
+
     }
     renderer.render(scene, camera);
 }
 renderer.setAnimationLoop(animate);
 
 function spaceshipMovement() {
-    if (spyShip) {
-        spyShip.position.set(spaceship.position.x, spaceship.position.y, spaceship.position.z);
-        spyShip.rotation.set(spaceship.rotation.x, spaceship.rotation.y, spaceship.rotation.z);
-    }
 
     rightPLight.position.set(spaceship.position.x -4, spaceship.position.y, spaceship.position.z+7);
     leftPLight.position.set(spaceship.position.x +4, spaceship.position.y, spaceship.position.z+7);
 
     camera.position.set(spaceship.position.x, spaceship.position.y + 30, spaceship.position.z + 100); // set the coords of camera
-    //camera.rotation.x = cameraControls.getDirection(new THREE.Vector3()).x * rotationSpeed;
-    //camera.rotation.y = cameraControls.getDirection(new THREE.Vector3()).y * rotationSpeed;
-    //camera.rotation.z = 0;
-
-    spaceship.position.z -= controls.speed;
 
     // keeping speed between assigned values
-    if (controls.speed > controls.maxSpeed) { controls.speed = controls.maxSpeed; }
-    if (controls.speed < controls.minSpeed) { controls.speed = controls.minSpeed; }
+    if (spaceshipBox.velocity.z > controls.minSpeed) { spaceshipBox.velocity.z = controls.minSpeed-1; }
+    if (spaceshipBox.velocity.z < controls.maxSpeed) { spaceshipBox.velocity.z = controls.maxSpeed+1; }
 
     // animation + control panel
     if (controls.speedUp) {
-        // todo: add motor animation
-        if (controls.speed <= controls.maxSpeed/2) {
-            controls.speed += 0.02;
-        } else if (controls.speed > controls.maxSpeed/2 && controls.speed <= controls.maxSpeed) {
-            controls.speed += 0.01;
+        if (controls.speed >= controls.maxSpeed/2) {
+            leftPLight.power += 0.1;
+            rightPLight.power += 0.1;
+            spaceshipBox.velocity.z -= 0.5;
+        } else if (controls.speed < controls.maxSpeed/2 && controls.speed >= controls.maxSpeed) {
+            leftPLight.power += 0.1;
+            rightPLight.power += 0.1;
+            spaceshipBox.velocity.z -= 0.25;
         }
     }
     if (controls.slowDown) {
-        // todo: add motor animation
         if (controls.speed > controls.minSpeed) {
-            controls.speed -= 0.01;
+            leftPLight.power -= 0.1;
+            rightPLight.power -= 0.1;
+            spaceshipBox.velocity.z += 0.5;
         }
     }
-    if (controls.right) {
-        if (spaceship.rotation.y > -4) {
-            spaceship.position.x += 1;
-            spaceship.rotation.z += Math.PI/450*controls.speed + 0.01;
+
+    let rotationSpeed = 0.01;
+    let quaternion = new CANNON.Quaternion();
+    if (controls.up && controls.totalRotation.x > controls.minRotation) {
+        spaceshipBox.velocity.y += 0.5;
+        quaternion.setFromAxisAngle(new CANNON.Vec3(1,0,0), -rotationSpeed);
+        spaceshipBox.quaternion = spaceshipBox.quaternion.mult(quaternion);
+        controls.totalRotation.x -= rotationSpeed;
+    }
+    if (controls.down && controls.totalRotation.x < controls.maxRotation) {
+        spaceshipBox.velocity.y -= 0.5;
+        quaternion.setFromAxisAngle(new CANNON.Vec3(1,0,0), rotationSpeed);
+        spaceshipBox.quaternion = spaceshipBox.quaternion.mult(quaternion);
+        controls.totalRotation.x += rotationSpeed;
+    }
+    if (!controls.down && !controls.up && controls.totalRotation.x !== 0) {
+        if (controls.totalRotation.x > 0) {
+            quaternion.setFromAxisAngle(new CANNON.Vec3(1,0,0), -rotationSpeed);
+            spaceshipBox.quaternion = spaceshipBox.quaternion.mult(quaternion);
+            controls.totalRotation.x -= rotationSpeed;
         }
-        else {
-            controls.right = false;
+        if (controls.totalRotation.x < 0) {
+            quaternion.setFromAxisAngle(new CANNON.Vec3(1,0,0), rotationSpeed);
+            spaceshipBox.quaternion = spaceshipBox.quaternion.mult(quaternion);
+            controls.totalRotation.x += rotationSpeed;
         }
     }
-    if (controls.left) {
-        if (spaceship.rotation.y < -2.3) {
-            spaceship.position.x -= 1;
-            spaceship.rotation.z -= Math.PI/450*controls.speed + 0.01;
+    if (!controls.left && !controls.right && controls.totalRotation.z !== 0) {
+        if (controls.totalRotation.z > 0) {
+            quaternion.setFromAxisAngle(new CANNON.Vec3(0,0,1), -rotationSpeed);
+            spaceshipBox.quaternion = spaceshipBox.quaternion.mult(quaternion);
+            controls.totalRotation.z -= rotationSpeed;
         }
-        else {
-            controls.left = false;
-        }
-    }
-    if (controls.up) {
-        if (spaceship.rotation.x < 0.5) {
-            spaceship.position.y += 1;
-            spaceship.rotation.x += Math.PI/180;
-        }
-    } else {
-        if (spaceship.rotation.x > 0) {
-            spaceship.rotation.x -= Math.PI/180;
+        if (controls.totalRotation.z < 0) {
+            quaternion.setFromAxisAngle(new CANNON.Vec3(0,0,1), rotationSpeed);
+            spaceshipBox.quaternion = spaceshipBox.quaternion.mult(quaternion);
+            controls.totalRotation.z += rotationSpeed;
         }
     }
-    if (controls.down) {
-        if (spaceship.rotation.x > -0.35) {
-            spaceship.position.y -= 1;
-            spaceship.rotation.x -= Math.PI/180;
-        }
-    } else {
-        if (spaceship.rotation.x < 0) {
-            spaceship.rotation.x += Math.PI/180;
-        }
+
+    if (controls.left && controls.totalRotation.z > controls.minRotation) {
+        spaceshipBox.velocity.x -= 0.5;
+        quaternion.setFromAxisAngle(new CANNON.Vec3(0,0,1), -rotationSpeed);
+        spaceshipBox.quaternion = spaceshipBox.quaternion.mult(quaternion);
+        controls.totalRotation.z -= rotationSpeed;
     }
-    if (!controls.left && !controls.right) {
-        if (spaceship.rotation.z > 0) {
-            spaceship.rotation.z -= Math.PI/450*controls.speed + 0.01;
-        }
-        if (spaceship.rotation.z < 0) {
-            spaceship.rotation.z += Math.PI/450*controls.speed + 0.01;
-        }
+    if (controls.right && controls.totalRotation.z < controls.maxRotation) {
+        spaceshipBox.velocity.x += 0.5;
+        quaternion.setFromAxisAngle(new CANNON.Vec3(0,0,1), rotationSpeed);
+        spaceshipBox.quaternion = spaceshipBox.quaternion.mult(quaternion);
+        controls.totalRotation.z += rotationSpeed;
     }
+    spaceshipBox.quaternion.y = 1;
 
 }
 
@@ -330,7 +223,6 @@ document.getElementById("Play").onclick = () => {
     cameraControls.lock();
 }
 // tracking mouse movements for camera controls
- var rotationSpeed = 0.02;
 window.addEventListener('click', function () {
     controls.lock();
 }, false);
@@ -341,6 +233,7 @@ window.addEventListener('resize', function () {
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
 });
+
 
 // event listener for character controls
 window.addEventListener('keydown', function (e) {
@@ -364,6 +257,9 @@ window.addEventListener('keydown', function (e) {
             break;
         case 'KeyS':
             controls.down = true;
+            break;
+        case 'Escape':
+            controls.pause = !controls.pause;
             break;
     }
 });
@@ -389,3 +285,47 @@ window.addEventListener('keyup', function (e) {
             break;
     }
 });
+
+// load obj
+function loadModel(modelName, path, position, scale, rotation, mass, castShadow, receiveShadow) {
+    let body, model;
+    assetLoader.load(path, function (gltf) {
+        model = gltf.scene;
+        // model.traverse((node) => {
+        //     if (node instanceof THREE.Mesh) {
+        //         node.castShadow = castShadow;
+        //         node.receiveShadow = receiveShadow;
+        //     }
+        // });
+        model.scale.set(scale.x, scale.y, scale.z);
+        model.position.copy(position);
+        model.rotation.copy(rotation);
+
+        const box = new THREE.Box3().setFromObject(model);
+        const size = box.getSize(new THREE.Vector3());
+        const shape = new CANNON.Box(new CANNON.Vec3(size.x / 2, size.y / 2, size.z / 2));
+        body = new CANNON.Body({ mass: mass });
+        body.addShape(shape);
+        body.position.copy(model.position);
+
+        body.quaternion.setFromEuler(model.rotation.x, model.rotation.y, model.rotation.z);
+
+        world.addBody(body);
+        model.userData.physicsBody = body;
+        scene.add(model);
+
+        if (modelName == "spaceship") {
+            spaceship = model;
+            spaceshipBox = body;
+            spaceshipBox.angularDamping = 1;
+            spaceshipBox.velocity = new CANNON.Vec3(0, 0, -25);
+        }
+        else if (modelName == "debris1") {
+            debris1 = model;
+            debris1Box = body;
+        }
+    }, undefined, function (error) {
+        console.error(error);
+    });
+}
+
